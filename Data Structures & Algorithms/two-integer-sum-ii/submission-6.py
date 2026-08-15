fdcf09c2d1809e37a96d3b07b29bf51df4bfa00d
Ravi Kumar Jha <91972538+rkj6124@users.noreply.github.com>
2026-08-15 18:08:11 +0530

    Add: two-integer-sum-ii - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        numset = defaultdict(list)
+        
+        for i in range(len(numbers)):
+            n = numbers[i]
+            numset[n].append(i + 1)
+        
+        for key in numset.keys():
+            if target - key in numset:
+                if key == target - key:
+                    return [numset[key][0], numset[key][1]]
+                return [numset[key][0], numset[target - key][0]]
